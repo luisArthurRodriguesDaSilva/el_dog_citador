40c3a969a2923ead53b0d3ce35987cfb6874e5a0
luis arthur <luisarthurlards03@gmail.com>
2022-12-30 16:26:04 -0300

    prontopro deploy

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ possibilidates = ['trending','dm','random']
 while True:
   escolha = possibilidates[randint(0,2)]
   print(escolha)
-  if (escolha == 'random') :
+  try:
+    if (escolha == 'random') :
       obj.escolha_random()
 
-  elif(escolha == 'autor') :
-      obj.escolha_autor()
+    elif(escolha == 'autor') :
+        obj.escolha_autor()
 
-  elif (escolha == 'trending') :
-      obj.escolha_trending()
+    elif (escolha == 'trending') :
+        obj.escolha_trending()
 
-  elif(escolha =='dm'):
-      obj.escolha_dm()
-
+    elif(escolha =='dm'):
+        obj.escolha_dm()
+  except Exception as e:
+    print(e)
   print('proximo')
   time.sleep(int(os.getenv('intervalo')))
--- ttApi.py
+++ ttApi.py
@@ -3,8 +3,8 @@ import os
 import dotenv
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-auth = tweepy.OAuthHandler(os.getenv('chave1'),os.getenv('chave2'))
-auth.set_access_token(os.getenv('chave3'),os.getenv('chave4'))
+auth = tweepy.OAuthHandler(os.getenv('citchave1'),os.getenv('citchave2'))
+auth.set_access_token(os.getenv('citchave3'),os.getenv('citchave4'))
 api = tweepy.API(auth)
 print((api.get_user(screen_name='noBugChapeu')._json['id_str']))
 
