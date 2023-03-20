6d30b8ca1c4e302a56190744d5f05cefca3441d8
luis arthur <luisarthurlards03@gmail.com>
2023-03-20 15:02:09 -0300

    refactor: :recycle: get trending

--- src/ttApi.py
+++ src/ttApi.py
@@ -1,6 +1,7 @@
 import tweepy
 import os
 import dotenv
+import json
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 BRAZIL_WOE_ID = 23424768
@@ -24,9 +25,20 @@ def imageToMyDm(image,text=' s '):
     attachment_type='media',
     attachment_media_id=media.media_id)
 
-def getActualTrending():
-  brazil_trends = api.get_place_trends(BRAZIL_WOE_ID)  
-  caras=[]
-  for i in range(int(len(brazil_trends[0]['trends'])/10)):
-      caras.append(str(brazil_trends[0]['trends'][i]['name'].replace('#','')))
+def getActualTrending(WOE_ID=BRAZIL_WOE_ID):
+  brazil_trends = api.get_place_trends(WOE_ID)  
+  caras=list(map(lambda x: x['name'].replace('#',''),brazil_trends[0]['trends']))
   return caras
+
+def getDMautors():
+  autors = []
+  messages = api.get_direct_messages(count=40)
+  print(len(messages))
+  for message in messages:
+    #print(json.dumps(message._json, indent=4))
+    messageText=message._json['message_create']['message_data']['text']
+    if(messageText[0:6]=="autor:" or messageText[0:6]== "Autor:"):
+        autor=messageText[6:len(messageText)]
+        autors.append(autor)
+  return autors
+
