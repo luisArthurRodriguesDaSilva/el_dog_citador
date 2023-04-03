ac63b14aff05dd849ded4eea522666a2b0b774b0
luis arthur <luisarthurlards03@gmail.com>
2023-04-03 17:48:32 -0300

    fix: :bug: parameters Try It bug

--- src/main.py
+++ src/main.py
@@ -23,13 +23,13 @@ while 1:
     trendingGuys = getActualTrending()
     for guy in trendingGuys:
         try:
-            makeFriends(guy)
+            makeFriends(q=guy)
             selectdPhase, selectedAuthor = getSelectedQuoteInfo(guy)
             formatted_text = f"{selectdPhase}\n__\n{selectedAuthor}"
             if len(formatted_text) > 280:
                 raise "grande demais"
             if not need_images():
-                twetIt(formatted_text)
+                twetIt(text=formatted_text)
             else:
                 play_image_mode(selectdPhase, selectedAuthor)
         except Exception as e:
